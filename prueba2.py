794f29c7ed5c136f989b99fb3cf46663a27cbb9a
alete89 <maildealete@gmail.com>
2018-01-02 13:08:53 -0300

    cohesive fx

--- prueba2.py
+++ prueba2.py
@@ -8,21 +8,26 @@ from PyQt4 import QtGui, QtCore
 class Window(QtGui.QWidget):
     def __init__(self, rows, columns):
         QtGui.QWidget.__init__(self)
-        
+
+        self.currentPath = ''
+        self.dataSet = []
         self.table = QtGui.QTableWidget(rows, columns, self)
         self.table.setSortingEnabled(True)
 
         # Buttons
+        self.buttonGetData = QtGui.QPushButton('getdatafromcsv', self)
         self.buttonOpen = QtGui.QPushButton('Open (show all records)', self)
         self.buttonMenu = QtGui.QPushButton('Filtro Menu', self)
         self.buttonSave = QtGui.QPushButton('Save', self)
-        self.buttonOpen.clicked.connect(self.ShowAllRecords)
+        self.buttonOpen.clicked.connect(self.getDataFromCsv)
+        self.buttonOpen.clicked.connect(self.ShowDataSet)
         self.buttonMenu.clicked.connect(self.menuFilter)
         self.buttonSave.clicked.connect(self.SaveCSV)
 
         # Layout
         layout = QtGui.QVBoxLayout(self)
         layout.addWidget(self.table)
+        layout.addWidget(self.buttonGetData)
         layout.addWidget(self.buttonOpen)
         layout.addWidget(self.buttonMenu)
         layout.addWidget(self.buttonSave)
@@ -32,7 +37,7 @@ class Window(QtGui.QWidget):
         if not path.isEmpty():
             with open(unicode(path), 'wb') as stream:
                 writer = csv.writer(stream, delimiter=';')
-                writer.writerow(self.header)
+                writer.writerow(self.getHeader(self.currentPath))
                 for row in range(self.table.rowCount()):
                     rowdata = []
                     for column in range(self.table.columnCount()):
@@ -44,39 +49,33 @@ class Window(QtGui.QWidget):
                             rowdata.append('')
                     writer.writerow(rowdata)
 
-    def ShowAllRecords(self):
+    def getDataFromCsv(self):
         path = QtGui.QFileDialog.getOpenFileName(self, 'Open File', '', 'CSV(*.csv)')
         if not path.isEmpty():
+            self.currentPath = path
             with open(unicode(path), 'rb') as stream:
-                self.table.setRowCount(0)
-                self.table.setColumnCount(0)
                 reader = csv.reader(stream, delimiter=';')
                 reader.next()
                 for rowdata in reader:
-                    row = self.table.rowCount()
-                    self.table.insertRow(row)
-                    self.table.setColumnCount(len(rowdata))
-                    for column, data in enumerate(rowdata):
-                        item = QtGui.QTableWidgetItem(data.decode('utf8'))
-                        self.table.setItem(row, column, item)
-            self.table.setHorizontalHeaderLabels(self.getHeader(path))
+                    self.dataSet.append(rowdata)
 
-    def menuFilter(self):
-        path = QtGui.QFileDialog.getOpenFileName(self, 'Open File', '', 'CSV(*.csv)')
-        if not path.isEmpty():
-            with open(unicode(path), 'rb') as stream:
-                self.table.setRowCount(0)
-                self.table.setColumnCount(0)
-                reader = csv.reader(stream, delimiter=';')
-                reader.next()
-                for rowdata in reader:
-                    row = self.table.rowCount()
-                    self.table.insertRow(row)
-                    self.table.setColumnCount(len(rowdata))
-                    for column, data in enumerate(rowdata):
-                        item = QtGui.QTableWidgetItem(data.decode('utf8'))
-                        self.table.setItem(row, column, item)
-            self.table.setHorizontalHeaderLabels(self.getHeader(path))
+    def ShowDataSet(self):
+        self.table.setRowCount(0)
+        self.table.setColumnCount(0)
+        for rowdata in self.dataSet:
+            row = self.table.rowCount()
+            self.table.insertRow(row)
+            self.table.setColumnCount(len(rowdata))
+            for column, data in enumerate(rowdata):
+                item = QtGui.QTableWidgetItem(data.decode('utf8'))
+                self.table.setItem(row, column, item)
+        self.table.setHorizontalHeaderLabels(self.getHeader(self.currentPath))
+
+    def menuFilter(self, columna, valor):
+        # SELECT DISTINCT SubMenu,Coordenada FROM TablaDeSecuencias where Menu="unmenu" and SubMenu is not null order by Coordenada
+
+
+        pass
 
     def getHeader(self, path):
         with open(unicode(path), 'rb') as stream:
